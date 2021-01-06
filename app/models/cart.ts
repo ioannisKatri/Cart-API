ad4f3fff904e01396b45c6b1f80d8dfb00b9269e
IoannisKatri <ioanniskatri@hotmail.com>
2021-01-06 14:16:29 +0200

    remove uncessary dependecies

--- app/models/cart.ts
+++ app/models/cart.ts
@@ -1,37 +1,36 @@
-import mongoose, {Schema} from "mongoose";
-import {ItemCartAttrs, itemCartSchema} from "./itemCartAttrs";
-
+import mongoose, { Schema } from "mongoose";
+import { ItemCartAttrs, itemCartSchema } from "./itemCartAttrs";
 
 interface CartAttrs {
-    userId: string,
-    cart: ItemCartAttrs[] | []
+  userId: string;
+  cart: ItemCartAttrs[] | [];
 }
 
 interface CartModel extends mongoose.Model<CartDoc> {
-    build(attrs: CartAttrs): CartDoc;
+  build(attrs: CartAttrs): CartDoc;
 }
 
 interface CartDoc extends mongoose.Document {
-    userId: string,
-    cart: ItemCartAttrs[] | [],
+  userId: string;
+  cart: ItemCartAttrs[] | [];
 }
 
 const cartSchema = new mongoose.Schema({
-    userId: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        index: true
-    },
-    cart: {
-        type: [itemCartSchema],
-    }
+  userId: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    index: true,
+  },
+  cart: {
+    type: [itemCartSchema],
+  },
 });
 
 cartSchema.statics.build = (attrs: CartAttrs) => {
-    return new Cart(attrs);
-}
+  return new Cart(attrs);
+};
 
-const Cart = mongoose.model<CartDoc, CartModel>('Cart', cartSchema);
+const Cart = mongoose.model<CartDoc, CartModel>("Cart", cartSchema);
 export default Cart;
