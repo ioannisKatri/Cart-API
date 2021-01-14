0f6e1e7636549e4218ecac3e4203ecd51d5f2ab9
IoannisKatri <ioanniskatri@hotmail.com>
2021-01-14 23:38:01 +0200

    Updated CartController for sending back a success: true boolean for the successful responses and renamed "data" response attribute to "payload"

--- app/controllers/cartController.ts
+++ app/controllers/cartController.ts
@@ -1,51 +1,44 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import Cart from "../models/cart";
-import {CartAlreadyExist} from "../configurations/errors/cart-already-exist";
-import {CartDontExist} from "../configurations/errors/cart-dont-exist";
+import { CartAlreadyExist } from "../configurations/errors/cart-already-exist";
+import { CartDontExist } from "../configurations/errors/cart-dont-exist";
 
 const getCart = async (req: Request, res: Response) => {
-    const user = await Cart.findOne({userId: req.user.id});
-    return res.status(200).json({response: user || null});
-}
+  const user = await Cart.findOne({ userId: req.user.id });
+  return res.status(200).json({ success: true, payload: user || null });
+};
 
 const createCart = async (req: Request, res: Response) => {
+  const cartExist = await Cart.findOne({ userId: req.user.id });
 
-    const cartExist = await Cart.findOne({userId: req.user.id});
+  if (cartExist) {
+    throw new CartAlreadyExist();
+  }
 
-    if (cartExist) {
-        throw new CartAlreadyExist();
-    }
-
-    try {
-        let cart = Cart.build(req.body);
-        cart = await cart.save();
-        return res.status(201).send(cart);
-    } catch (err) {
-        throw new Error(err);
-    }
-}
+  try {
+    let cart = Cart.build(req.body);
+    cart = await cart.save();
+    return res.status(201).json({ success: true, payload: cart });
+  } catch (err) {
+    throw new Error(err);
+  }
+};
 
 const updateCart = async (req: Request, res: Response) => {
-    const cart = await Cart.findOne({userId: req.user.id});
+  const cart = await Cart.findOne({ userId: req.user.id });
 
-    if (!cart) {
-        throw new CartDontExist();
-    }
+  if (!cart) {
+    throw new CartDontExist();
+  }
 
-    // TODO create business logic for updating the cart
-    try {
-        cart.cart = req.body.cart;
-        await cart.save();
-        return res.status(200).json({response: cart});
-    } catch (err) {
-        console.log(err.message)
-        return res.status(400).json({message: err.errors});
-    }
-}
+  try {
+    cart.cart = req.body.cart;
+    await cart.save();
+    return res.status(200).json({ success: true, payload: cart });
+  } catch (err) {
+    console.log(err.message);
+    return res.status(400).json({ success: false, message: err.errors });
+  }
+};
 
-export {
-    getCart,
-    createCart,
-    updateCart,
-}
-
+export { getCart, createCart, updateCart };
