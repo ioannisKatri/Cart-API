5b19ccba70e6342af49dded6f9f57c3e2e3f795e
IoannisKatri <ioanniskatri@hotmail.com>
2021-01-16 18:11:31 +0200

    - modified package.json test script export NODE_ENV=test in order to run logger during the test in the console
    Added rule in app.ts to utilize console logging if the environment is not test

--- app/app.ts
+++ app/app.ts
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === "production") {
     app.use(expressWinston.logger(logConfigProduction));
-} else {
+} else if (process.env.NODE_ENV !== "test"){
     app.use(expressWinston.logger(logConfigDev));
 }
 
